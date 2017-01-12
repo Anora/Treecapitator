413134dbf32fa58df7fcb1ff91365f4f27119bdc
Matjaz Rihtar <matjaz@eunet.si>
2017-01-13 00:30:27 +0100

    1.11.2 update

--- src/main/java/bspkrs/treecapitator/util/Reference.java
+++ src/main/java/bspkrs/treecapitator/util/Reference.java
@@ -4,7 +4,6 @@ public class Reference
 {
     public static final String MODID                              = "treecapitator";
     public static final String NAME                               = "TreeCapitator";
-    public static final String VERSION                            = "1.41";
     public static final String PROXY_COMMON                       = "bspkrs.treecapitator.CommonProxy";
     public static final String PROXY_CLIENT                       = "bspkrs.treecapitator.ClientProxy";
     public static final String GUI_FACTORY                        = "bspkrs.treecapitator.fml.gui.ModGuiFactoryHandler";
